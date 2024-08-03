411313baeb39b4aad470877d0a5751962efc4652
Vinoth_M <117703625+Vinoth-M-08@users.noreply.github.com>
2024-08-03 20:33:34 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+    document.getElementById('menu-toggle').addEventListener('click', function() {
+        var navList = document.getElementById('nav-list');
+        navList.classList.toggle('active');
+    });
+</script>
